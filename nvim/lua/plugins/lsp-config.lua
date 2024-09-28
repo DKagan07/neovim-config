e7c293aec32aa0f35c2315e64d2881ce6820f685
David Kagan <david.kagan07@gmail.com>
2024-09-28 11:50:53 -0400

    Update plugins
    Updated all plugins with Lazy. TSServer was deprecated, changed to ts_ls
    per docs

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,68 +1,68 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"eslint",
-					"gopls",
-					"lua_ls",
-					"rust_analyzer",
-					"tsserver",
-					"pyright",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "eslint",
+                    "gopls",
+                    "lua_ls",
+                    "rust_analyzer",
+                    "ts_ls",
+                    "pyright",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
 
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.gopls.setup({
+                capabilities = capabilities,
+            })
 
-			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.rust_analyzer.setup({
+                capabilities = capabilities,
+            })
 
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities,
+            })
 
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+            })
 
-			lspconfig.eslint.setup({
-				-- settings = {
-				-- 	packageManager = "npm",
-				-- },
-				on_attach = function(client, bufnr)
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						buffer = bufnr,
-						command = "EslintFixAll",
-					})
-				end,
-			})
+            lspconfig.eslint.setup({
+                -- settings = {
+                -- 	packageManager = "npm",
+                -- },
+                on_attach = function(client, bufnr)
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        command = "EslintFixAll",
+                    })
+                end,
+            })
 
-			-- Keymaps
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            -- Keymaps
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
